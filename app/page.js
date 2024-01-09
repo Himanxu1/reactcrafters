c4e8d9c2d1561621e995c184ea733b0a00da67b2
himanxu1 <technophilic000@gmail.com>
2024-01-09 16:14:52 +0530

    comma-added

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       <div className=" text-white flex flex-col justify-center mt-10  lg:mx-64 mx-10 text-center">
         <p className="font-bold  lg:text-6xl text-2xl">Elevate Your Brand with  <span  className="bg-gradient-to-r from-violet-200 to-purple-600 text-transparent bg-clip-text">Swift Development</span> , <span className="bg-gradient-to-r from-violet-200 to-purple-600 text-transparent bg-clip-text" >Seamless Experiences</span> , and <span className="bg-gradient-to-r from-violet-200 to-purple-600 text-transparent bg-clip-text"> Scalable Growth!</span></p>
         <div className="">
-          <p className="text-center mt-10 lg:text-xl text-lg text-gray-400">As a leading React.js development agency, we specialize in crafting seamless, high-performance web applications that redefine user experiences. Whether you're a startup looking to make a splash or an established business seeking a digital transformation, our team of expert React.js developers is here to bring your vision to life.</p>
+          <p className="text-center mt-10 lg:text-xl text-lg text-gray-400">As a leading React.js development agency, we specialize in crafting seamless, high-performance web applications that redefine user experiences. Whether you&apos;re a startup looking to make a splash or an established business seeking a digital transformation, our team of expert React.js developers is here to bring your vision to life.</p>
 
           <div className="flex justify-center mt-4">
             <Link href="/contact"  className="px-2 py-2 bg-purple-600 rounded text-md hover:border-2 hover:border-purple-600 hover:bg-transparent " >Contact Us</Link>
