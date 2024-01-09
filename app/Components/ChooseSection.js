85f5a38691cb609fae023df2753a4f08f5da3465
himanxu1 <technophilic000@gmail.com>
2024-01-09 16:13:21 +0530

    bug-fix

--- app/Components/ChooseSection.js
+++ app/Components/ChooseSection.js
@@ -7,10 +7,10 @@ const ChooseSection = () => {
     <h1 className=" text-2xl lg:text-4xl font-bold text-center mt-20"><span><strike>Why</strike> </span> Choose ReactCrafters?</h1>
     <div className="lg:grid lg:grid-cols-3 gap-10 p-4 place-items-center lg:mx-28 mx-10 mt-10  ">
         {
-            whyChooseUs.map((item)=>{
+            whyChooseUs.map((item,index)=>{
                 return (
                     <div>
-                     <ChooseCard item={item} key={item.title} />    
+                     <ChooseCard item={item} key={index} />    
                     </div>
                 )
             })
